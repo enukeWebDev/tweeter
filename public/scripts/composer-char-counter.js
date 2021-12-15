55d82d5129f67180e2772f1ef5d0008325e8c3ba
Roderick Nuque <enuke4@gmail.com>
2021-12-15 03:49:22 +0000

    Revert "cleaning up my app before adding more functionality"
    This reverts commit ba5a60ad1a0a0b828716983e5d846a561d6122d7.

--- public/scripts/composer-char-counter.js
+++
@@ -1,9 +0,0 @@
-console.log('Tweeter is getting ready...');
-
-$(document).ready(function() {
-  console.log('You can start Tweet-ing...');
-
-  $('#tweet-text').keyup(function() {
-    console.log('Clicked!!!');
-  });
-});
